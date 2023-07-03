84be4dcc358292da0ea6085338db56dfc91dcfc5
frant <frantisek.sereda@gmail.com>
2023-07-03 17:26:01 +0200

    Nicer gui. Comparison not working for same key/diffrent value situations

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 import xmltodict
 import json
+import pandas
 
 app = Flask(__name__)
 
@@ -33,6 +34,14 @@ def flatten_json(y):
     return out
 
 
+# def make_comparable_keys(dictionary: dict):
+#     keys_list = list(dictionary.keys())
+#     comparable_keys_list = []
+#
+#     for key in keys_list:
+#
+
+
 def make_nice_keys(dictionary: dict, dividing_string: str):
     keys_list = list(dictionary.keys())
     nice_keys_list = []
@@ -44,6 +53,7 @@ def make_nice_keys(dictionary: dict, dividing_string: str):
     new_nice_dictionary = dict(zip(nice_keys_list, list(dictionary.values())))
 
     return new_nice_dictionary
+
 
 def same_value_key_pairs(dictionary1: dict, dictionary2: dict):
     d1_keys = set(dictionary1.keys())
@@ -60,16 +70,18 @@ def different_value_key_pairs(dictionary1: dict, dictionary2: dict):
     different = {o: (dictionary1[o], dictionary2[o]) for o in shared_keys if dictionary1[o] != dictionary2[o]}
     return different
 
+
+def convert_all_values_to_string(dictionary: dict):
+    transformed_dictionary = dictionary
+    for key in transformed_dictionary:
+        transformed_dictionary[key] = str(transformed_dictionary[key])
+    return transformed_dictionary
+
+
 @app.route("/upload")
 def upload_file():
-    global uploaded_xml
-    uploaded_xml = "Zatím nic"
-    global uploaded_json
-    uploaded_json = "Ještě taky nic"
-    global uploaded_are_equal
-    uploaded_are_equal = "Nevíme"
-    return render_template("upload.html", xml_to_show=uploaded_xml, json_to_show=uploaded_json,
-                           same_to_show=uploaded_are_equal)
+
+    return render_template("upload.html")
 
 
 @app.route("/uploader-oba", methods=['GET', 'POST'])
@@ -78,25 +90,33 @@ def upload_oba():
         uploaded_xml = request.files["file1"].read().decode("utf-8")
         uploaded_json = request.files["file2"].read().decode("utf-8")
         uploaded_xml_dict = make_nice_keys(dictionary=flatten_json(xmltodict.parse(uploaded_xml)), dividing_string=":")
-        uploaded_json_dict = make_nice_keys(dictionary=flatten_json(json.loads(uploaded_json)), dividing_string="_")
+        uploaded_json_dict = convert_all_values_to_string(make_nice_keys(dictionary=flatten_json(json.loads(uploaded_json)), dividing_string="_"))
 
         same_values = same_value_key_pairs(uploaded_json_dict, uploaded_xml_dict)
         different_values = different_value_key_pairs(uploaded_json_dict, uploaded_xml_dict)
 
-        # tohle už je na nic, byl to úvodní pokus o nějakou logiku
-        if uploaded_xml == uploaded_json:
-            uploaded_are_equal = "Ano, jsou stejné"
-        if uploaded_xml != uploaded_json:
-            uploaded_are_equal = "Ne, nejsou stejné"
+        same_values_panda_df = pandas.DataFrame(data=same_values).rename(columns={0: "Hodnoty jsou stejné pro:"})
+        table_same_values = same_values_panda_df.to_html(classes=["table",
+                                                                  "table-bordered",
+                                                                  "table-striped",
+                                                                  "table-hover"], index=False)
+
+        different_values_panda_df = pandas.DataFrame(data=different_values)
+        table_different_values = different_values_panda_df.T.rename(columns={0: "JSON:", 1: "XML"}).to_html(classes=["table",
+                                                                                                                     "table-bordered",
+                                                                                                                     "table-striped",
+                                                                                                                     "table-hover",
+                                                                                                                     "width="])
 
         return render_template("upload.html",
                                xml_to_show=uploaded_xml,
                                json_to_show=uploaded_json,
-                               same_to_show=uploaded_are_equal,
                                uploaded_xml_dict=uploaded_xml_dict,
                                uploaded_json_dict=uploaded_json_dict,
                                same_values=same_values,
-                               different_values=different_values
+                               different_values=different_values,
+                               table_same_values=table_same_values,
+                               table_different_values=table_different_values
                                )
 
 
